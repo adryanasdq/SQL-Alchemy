f25e8d470d5121e1aed447932b56a0e42553a5b6
Adryan Ashidiq <adryan@upi.edu>
2023-06-12 17:09:47 +0700

    'com1'

--- migratetest.py
+++ migratetest.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5433/testing'
@@ -12,12 +13,39 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=True)
+    # age = db.Column(db.Integer, nullable=True)
 
-def perform_migration():
-    with app.app_context():
-        migrate.init_app(app)
-        migrate.migrate()
-        migrate.upgrade()
+    def __repr__(self):
+        return f"<user {self.name}>"
 
-perform_migration()
+@app.route('/')
+def execute_query():
+    query_string = text(
+        '''
+        SELECT *
+        FROM users
+        WHERE id > 1
+        '''
+    )
+    result = db.session.execute(query_string)
+    # Process the result as needed
+    response = [
+        {
+            "id": u.id,
+            "name": u.name,
+            "email": u.email
+        } for u in result
+    ]
+
+    return {'data': response}
+
+# def perform_migration():
+#     with app.app_context():
+#         migrate.init_app(app)
+#         migrate.migrate()
+#         migrate.upgrade()
+
+# perform_migration()
+
+if __name__ == '__main__':
+    app.run(debug=True)
